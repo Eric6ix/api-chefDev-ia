3680666013564a0cf66f8dd9ad98cc4118825b92
unknown <Eric6ix>
2026-06-03 17:59:50 -0300

    config dotenv em index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
-const PORT = 3000;
+const PORT_SERVER = process.env.PORT_SERVER;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT_SERVER, () => {
+  console.log(`Server is running on port ${PORT_SERVER}`);
 });
