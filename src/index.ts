d1e2f26446fa4f6f34081aba379e6f17fa6e2a30
unknown <Eric6ix>
2026-06-05 13:50:02 -0300

    feat: add TypeScript implementação para configuração do servidor

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import geminiRouter from "./agent/gemini.service.js";
+
 const app = express();
+
 app.use(express.json());
 app.use(geminiRouter);
+
 const PORT_SERVER = Number(process.env.PORT_SERVER);
+
 app.listen(PORT_SERVER, () => {
-    console.log(`Server is running on port ${PORT_SERVER}`);
+  console.log(`port ${PORT_SERVER}`);
 });
