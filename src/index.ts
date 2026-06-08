0bcefce313b5d19bb59f6d1cb3fdc09a6e9c3e01
unknown <Eric6ix>
2026-06-08 09:04:47 -0300

    add dependence cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import "dotenv/config";
 import geminiRouter from "./agent/gemini.service.js";
-
+import cors from "cors";
 const app = express();
-
 app.use(express.json());
 app.use(geminiRouter);
-
+app.use(cors());
 const PORT_SERVER = Number(process.env.PORT_SERVER);
 
 app.listen(PORT_SERVER, () => {
